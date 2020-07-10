774e0ba465e6be80e4899339f1513a301fb9cf77
Masamichi Takano <masia02@gmail.com>
2020-07-10 21:30:15 +0900

    mail

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,7 @@
 export const GITHUB_PROFILE_LINK = 'https://github.com/masia02'
 export const LINKEDIN_PROFILE_LINK = 'https://www.linkedin.com/in/masia02/'
 export const FACEBOOK_PROFILE_LINK = 'https://www.facebook.com/cipherweb/'
-export const GMAIL_LINK = 'mailto:info@cipher-web.com'
+export const GMAIL_LINK = 'https://cipherweb.substack.com/'
 
 export const JOB_DATA = {
     name: 'Cryptovoxcel',
