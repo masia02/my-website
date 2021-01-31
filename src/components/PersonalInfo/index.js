2e263996d606be8e31f80b46e04697d5d53417bb
masia02.eth <masia02@gmail.com>
2021-02-01 00:51:58 +0900

    Update index.js

--- src/components/PersonalInfo/index.js
+++ src/components/PersonalInfo/index.js
@@ -25,18 +25,18 @@ const PersonalInfo = () => {
         <h1>CipherWeb LLC</h1>
         <h3>
           <span>社名　サイファーウェブ合同会社<br/>
-          代表社員　<a target="_blank" rel="noopener noreferrer" href="https://masia02.eth.link">高野勝通</a><br/>
+          代表社員　<a target="_blank" rel="noopener noreferrer" href="https://masia02.hns.to">高野勝通</a><br/>
           Date of Establishment October 15, 2018<br/>
           Digital Creation @ </span>
           <a target="_blank" rel="noopener noreferrer" href={JOB_DATA.url}>{JOB_DATA.name}</a>
         </h3>
         <div className="separator"></div>
         <p>
+          デジタル資産入門：<a target="_blank" rel="noopener noreferrer" href="https://getbits.net">Getbits</a>,<a target="_blank" rel="noopener noreferrer" href="https://note.com/masia02/m/mfbd4966b3668">dWebXRマガジン</a><br/>
+          CryptoART：<a target="_blank" rel="noopener noreferrer" href="https://mintbase.io/my-market/0x4d38eea6619b4159498b31df9623000f2e6ef25e">mintbase</a>,<a target="_blank" rel="noopener noreferrer" href="https://opensea.io/category/cipherweb-arts/">OpenSea</a>,<a target="_blank" rel="noopener noreferrer" href="https://app.rarible.com/cipherwebllc/collectibles">Rarible</a><br/>
+          Shop：<a target="_blank" rel="noopener noreferrer" href="https://cipherweb.base.ec/">BASE</a><br/>
           Webコンサルティング：<a target="_blank" rel="noopener noreferrer" href="https://note.mu/masia02/n/n6e2dada5a2dc">note</a><br/>
           WebAppStore：<a target="_blank" rel="noopener noreferrer" href="https://appfav.net">Appfav</a><br/>
-          デジタル資産入門：<a target="_blank" rel="noopener noreferrer" href="https://getbits.net">Getbits</a>,<a target="_blank" rel="noopener noreferrer" href="https://dapps-play.net">dApps-Play</a><br/>
-          CryptoART：<a target="_blank" rel="noopener noreferrer" href="https://mintbase.io/my-market/0x4d38eea6619b4159498b31df9623000f2e6ef25e">mintbase</a>,<a target="_blank" rel="noopener noreferrer" href="https://opensea.io/category/cipherweb-arts/">OpenSea</a><br/>
-          グッズ販売：<a target="_blank" rel="noopener noreferrer" href="https://cipherweb.base.ec/">BASE</a><br/>
           <b>Contact</b>.<a target="_blank" rel="noopener noreferrer" href="https://line.me/R/ti/p/%40zkz7445k">LINE</a>,<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cipherwebllc">Twitter</a>,<a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cipher_web">instagram</a>
         </p>
         <button className="toggle-overlay-button" onClick={handleClick}>Explore →</button>
