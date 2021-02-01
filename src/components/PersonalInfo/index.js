02155f60f1184abf29c41bcdd986f40b86f9e8ad
masia02.eth <masia02@gmail.com>
2021-02-01 20:03:22 +0900

    Update index.js

--- src/components/PersonalInfo/index.js
+++ src/components/PersonalInfo/index.js
@@ -35,7 +35,7 @@ const PersonalInfo = () => {
           暗号資産入門：<a target="_blank" rel="noopener noreferrer" href="https://getbits.net">Getbits</a>,<a target="_blank" rel="noopener noreferrer" href="https://note.com/masia02/m/mfbd4966b3668">dWebXRマガジン</a><br/>
           CryptoART：<a target="_blank" rel="noopener noreferrer" href="https://opensea.io/category/cipherweb-arts/">OpenSea</a>,<a target="_blank" rel="noopener noreferrer" href="https://mintbase.io/my-market/0x4d38eea6619b4159498b31df9623000f2e6ef25e">mintbase</a>,<a target="_blank" rel="noopener noreferrer" href="https://app.rarible.com/cipherwebllc/collectibles">Rarible</a><br/>
           Shop：<a target="_blank" rel="noopener noreferrer" href="https://shop.cipher-web.com/">3DモデルDL＆グッズ販売</a><br/>
-          Webコンサルティング：<a target="_blank" rel="noopener noreferrer" href="https://note.mu/masia02/n/n6e2dada5a2dc">note</a>,<a target="_blank" rel="noopener noreferrer" href="https://medium.com/cipher-web">Medium</a><br/>
+          Webコンサルティング：<a target="_blank" rel="noopener noreferrer" href="https://medium.com/cipher-web">Medium</a><br/>
           WebAppStore：<a target="_blank" rel="noopener noreferrer" href="https://appfav.net">Appfav</a><br/>
           <b>Contact：</b><a target="_blank" rel="noopener noreferrer" href="https://line.me/R/ti/p/%40zkz7445k">LINE</a>,<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cipherwebllc">Twitter</a>,<a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cipher_web">instagram</a>
         </p>
