c9470a87387e3716af6c6907bcafce7b456ee466
masia02.eth <masia02@gmail.com>
2021-07-23 12:06:38 +0900

    Update index.js

--- src/components/PersonalInfo/index.js
+++ src/components/PersonalInfo/index.js
@@ -26,6 +26,7 @@ const PersonalInfo = () => {
         <h3>
           <span>社名　サイファーウェブ合同会社<br/>
           代表社員　<a target="_blank" rel="noopener noreferrer" href="https://masia02.hns.to">高野勝通</a><br/>
+          オープンメタバースに関わっていく会社<br/>
           <a target="_blank" rel="noopener noreferrer" href="https://dwebxr.on.fleek.co/">dWebXR</a> Tester. CryptoArtist Support.<br/>
           Digital Creation @ </span>
           <a target="_blank" rel="noopener noreferrer" href={JOB_DATA.url}>{JOB_DATA.name}</a>
@@ -34,8 +35,6 @@ const PersonalInfo = () => {
         <p>
           暗号資産入門：<a target="_blank" rel="noopener noreferrer" href="https://getbits.net">Getbits</a>,<a target="_blank" rel="noopener noreferrer" href="https://note.com/masia02/m/mfbd4966b3668">dWebXRマガジン</a><br/>
           CryptoART：<a target="_blank" rel="noopener noreferrer" href="https://app.cargo.build/showcase/dwebxr-labo/cc4c2c/for-sale?filter=%7B%7D">Cargo</a>,<a target="_blank" rel="noopener noreferrer" href="https://opensea.io/accounts/CipherWeb-Arts/">OpenSea</a><br/>
-          Shop：<a target="_blank" rel="noopener noreferrer" href="https://shop.cipher-web.com/">3DモデルDL＆グッズ販売</a><br/>
-          Webコンサルティング：<a target="_blank" rel="noopener noreferrer" href="https://medium.com/cipher-web">Medium</a><br/>
           WebAppStore：<a target="_blank" rel="noopener noreferrer" href="https://appfav.net">Appfav</a><br/>
           <b>Contact：</b><a target="_blank" rel="noopener noreferrer" href="https://line.me/R/ti/p/%40zkz7445k">LINE</a>,<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cipherwebllc">Twitter</a>,<a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cipher_web">instagram</a>
         </p>
