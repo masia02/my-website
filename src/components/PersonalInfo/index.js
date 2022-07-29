3f671ce7e635b58cfcffbd2e74585c1f7c148658
masia02.eth <masia02@gmail.com>
2022-07-30 06:58:57 +0900

    Update index.js

--- src/components/PersonalInfo/index.js
+++ src/components/PersonalInfo/index.js
@@ -25,15 +25,15 @@ const PersonalInfo = () => {
         <h1>CipherWeb LLC</h1>
         <h3>
           <span>社名　　<a target="_blank" rel="noopener noreferrer" href="https://linktr.ee/cipherweb">サイファーウェブ合同会社</a><br/>
-          代表社員 高野勝通<br/>
+          代表社員 <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/masia02">高野勝通</a><br/>
           <a target="_blank" rel="noopener noreferrer" href="https://jpx7m-miaaa-aaaad-qahpa-cai.ic.fleek.co/">Web3メタバース</a>に関わっていく会社<br/>
-          <a target="_blank" rel="noopener noreferrer" href="https://dwebxr.on.fleek.co/">dWebXR</a> Tester. CryptoArtist Support.<br/>
+          <a target="_blank" rel="noopener noreferrer" href="https://dwebxr.on.fleek.co/">dWebXR</a> CryptoArtist & Support.<br/>
           Digital Creation @ </span>
           <a target="_blank" rel="noopener noreferrer" href={JOB_DATA.url}>{JOB_DATA.name}</a>
         </h3>
         <div className="separator"></div>
         <p>
-          暗号資産入門：<a target="_blank" rel="noopener noreferrer" href="https://note.com/masia02/m/mfbd4966b3668">dWebXRマガジン</a><br/>
+          Media：<a target="_blank" rel="noopener noreferrer" href="https://dapps-play.net/">dapps-play</a>,<a target="_blank" rel="noopener noreferrer" href="https://mirror.xyz/cipherweb.eth">mirror</a>,<a target="_blank" rel="noopener noreferrer" href="https://dwqte-viaaa-aaaai-qaufq-cai.ic0.app/@masia02">DSocial</a><br/>
           CryptoARTs：<a target="_blank" rel="noopener noreferrer" href="https://opensea.io/accounts/CipherWeb-Arts/">OpenSea</a>,<a target="_blank" rel="noopener noreferrer" href="https://tofunft.com/user/0xe7C33E18a0324B7C7Db322EDae3Bdbc84Db3A83E/items/in-wallet">TOFUNFT</a><br/>
           Collections：<a target="_blank" rel="noopener noreferrer" href="https://polygonlow.xyz/">Polygonlow</a>,<a target="_blank" rel="noopener noreferrer" href="https://opensea.io/collection/momomitsuki">Momo Mitsuki</a><br/>
           Contact：<a target="_blank" rel="noopener noreferrer" href="https://discord.gg/Cfywb3aNWg">Discord</a>,<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cipherwebllc">Twitter</a>,<a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cipher_web">instagram</a>,<a target="_blank" rel="noopener noreferrer" href="https://line.me/R/ti/p/%40zkz7445k">LINE</a>
