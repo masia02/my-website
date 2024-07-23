0739a2c4a8c5795492b8e9cffb84921293e3173c
masia02.eth <masia02@gmail.com>
2024-07-24 02:48:02 +0900

    Update index.js

--- src/components/PersonalInfo/index.js
+++ src/components/PersonalInfo/index.js
@@ -26,13 +26,14 @@ const PersonalInfo = () => {
         <h3>
           <span>社名　　<a target="_blank" rel="noopener noreferrer" href="https://linktr.ee/cipherweb">サイファーウェブ合同会社</a><br/>
           代表社員 <a target="_blank" rel="noopener noreferrer" href="https://masia02.eth.limo">高野勝通</a><br/>
+          ＩＴソフトウェアの導入、活用支援<br/>
           <a target="_blank" rel="noopener noreferrer" href="https://hyperfy.io/cipherweb">Web3メタバース</a>.AIに関わっていく会社<br/>
           <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/dwebxr_lab">dWebXR</a> CryptoArtist & Support.</span><br/>
           <a target="_blank" rel="noopener noreferrer" href="https://dwebxr.online">dWebXR Lab</a> ⇒ <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/85JVzJaatT">DAO</a>へ
         </h3>
         <div className="separator"></div>
         <p>
-          Media：<a target="_blank" rel="noopener noreferrer" href="https://mora.app/planet/vk6gi-nqaaa-aaaan-qdgwq-cai">Mora</a>,<a target="_blank" rel="noopener noreferrer" href="https://appfav.net">Appfav</a>,<a target="_blank" rel="noopener noreferrer" href="https://dwqte-viaaa-aaaai-qaufq-cai.ic0.app/@masia02">DSocial</a><br/>
+          Media：<a target="_blank" rel="noopener noreferrer" href="https://mora.app/planet/vk6gi-nqaaa-aaaan-qdgwq-cai">Mora</a>,<a target="_blank" rel="noopener noreferrer" href="https://appfav.net">Appfav</a><br/>
           NFT Project：<a target="_blank" rel="noopener noreferrer" href="https://polygonlow.xyz/">PolygonLow</a>,<a target="_blank" rel="noopener noreferrer" href="https://momomitsuki.on.fleek.co/">MomoMitsuki</a><br/>
           Contact：<a target="_blank" rel="noopener noreferrer" href="https://discord.gg/Cfywb3aNWg">Discord</a>,<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cipherwebllc">Twitter</a>,<a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cipher_web">instagram</a>,<a target="_blank" rel="noopener noreferrer" href="https://line.me/R/ti/p/%40zkz7445k">LINE</a>,<a target="_blank" rel="noopener noreferrer" href="https://bsky.app/profile/cipher-web.com">bsky</a><br />
           <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSe1QVKSQacI2nsYNIezSqhVh7dg04xJa_jF_whcD6Jpm0KXwA/viewform">弊社へのお問合せ</a>
