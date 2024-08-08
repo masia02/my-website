45f03979523e4250796c9fb08f03ece1582dd236
masia02.eth <masia02@gmail.com>
2024-08-08 14:45:59 +0900

    Update index.js

--- src/components/PersonalInfo/index.js
+++ src/components/PersonalInfo/index.js
@@ -35,8 +35,7 @@ const PersonalInfo = () => {
         <p>
           Media：<a target="_blank" rel="noopener noreferrer" href="https://mora.app/planet/vk6gi-nqaaa-aaaan-qdgwq-cai">Mora</a>,<a target="_blank" rel="noopener noreferrer" href="https://appfav.net">Appfav</a><br/>
           NFT Project：<a target="_blank" rel="noopener noreferrer" href="https://polygonlow.xyz/">PolygonLow</a>,<a target="_blank" rel="noopener noreferrer" href="https://momomitsuki.on.fleek.co/">MomoMitsuki</a><br/>
-          Contact：<a target="_blank" rel="noopener noreferrer" href="https://discord.gg/Cfywb3aNWg">Discord</a>,<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cipherwebllc">Twitter</a>,<a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cipher_web">instagram</a>,<a target="_blank" rel="noopener noreferrer" href="https://line.me/R/ti/p/%40zkz7445k">LINE</a>,<a target="_blank" rel="noopener noreferrer" href="https://bsky.app/profile/cipher-web.com">bsky</a><br/>
-          本店所在地：新潟県新潟市中央区高美町４番１４号ホーミーハウス２０１号
+          Contact：<a target="_blank" rel="noopener noreferrer" href="https://discord.gg/Cfywb3aNWg">Discord</a>,<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cipherwebllc">Twitter</a>,<a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cipher_web">instagram</a>,<a target="_blank" rel="noopener noreferrer" href="https://line.me/R/ti/p/%40zkz7445k">LINE</a>,<a target="_blank" rel="noopener noreferrer" href="https://bsky.app/profile/cipher-web.com">bsky</a>
         </p>
         <button className="toggle-overlay-button" onClick={handleClick}>Explore →</button>
       </div>
