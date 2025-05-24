3414da0b47d9e58f04b57ccdc6ab7b9447a69cea
masia02 <masia02@gmail.com>
2025-05-25 06:45:49 +0900

    Delete src directory

--- src/components/constants.js
+++
@@ -1,9 +0,0 @@
-export const GITHUB_PROFILE_LINK = 'https://github.com/cipherwebllc'
-export const LINKEDIN_PROFILE_LINK = 'https://www.linkedin.com/in/masia02/'
-export const FACEBOOK_PROFILE_LINK = 'https://www.facebook.com/cipherweb/'
-export const GMAIL_LINK = 'https://cipherweb.substack.com/'
-
-export const JOB_DATA = {
-    name: 'Voxcels',
-    url: 'https://www.voxels.com/play?coords=N@608E,276S'
-  }
