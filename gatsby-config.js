0cf4c6932ba66fb03d1f77c2d0a9479f2605794a
masia02 <masia02@gmail.com>
2025-06-01 16:48:58 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `サイファーウェブ合同会社`,
-    description: `フリーランス(個人企業法人)Webコンサルティング・dWebXR・クリプトアーティスト・ブロックチェーンテクノロジーが創る分散社会の思想家`,
+    description: `ITソフトウェア導入・web3・メタバース・AIエージェント関連事業`,
     author: `CipherWeb LLC`,
   },
   plugins: [
